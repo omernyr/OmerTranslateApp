a8f311a10ac42b050dfe9c29a89cb78c936cfc7d
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-06-15 15:06:39 +0300

    add request headers and postMethod in Constant

--- OmerTranslateApp/NetworkingManager/NetworkConstant.swift
+++ OmerTranslateApp/NetworkingManager/NetworkConstant.swift
@@ -13,5 +13,11 @@ class NetworkConstant {
     
     private init() {}
     
+    static let headers = [
+           "content-type": "application/json",
+           "X-RapidAPI-Key": "fe83b38e34mshc8c70c0d52b8a61p1ac627jsnbdb546853370",
+           "X-RapidAPI-Host": "microsoft-translator-text.p.rapidapi.com"
+       ]
     
+    static let postMethod = "POST"
 }
