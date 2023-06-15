e5980c8a7d88144216f51b7eabb3e27f05993f4b
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-06-15 15:00:52 +0300

    add ConvertedTextView

---
+++ OmerTranslateApp/View/ConvertedTextView.swift
@@ -0,0 +1,34 @@
+//
+//  ConvertedTextView.swift
+//  OmerTranslateApp
+//
+//  Created by macbook pro on 15.06.2023.
+//
+
+import UIKit
+
+class ConvertedTextView: UITextView {
+
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        setupTextView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupTextView()
+        
+    }
+
+    private func setupTextView() {
+        
+       self.font = UIFont(name: "Gilroy-Medium", size: 19.0)
+       self.layer.cornerRadius = 10.0
+       self.textColor = .black
+       self.textAlignment = .center
+       self.backgroundColor = UIColor(hexString: "FEF9EF")
+    }
+    
+    
+
+}
